9b151542c52bfd2c4d753957e05bcf53c9224955
valeria campilongo <cvaleriac@hotmail.it>
2019-03-19 15:51:57 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,18 @@
 class SessionsController < ApplicationController
   def new
-  end
-
-  def home
+    @user = User.new
   end
 
   def create
-    @user = User.find_by(username: params[:username])
-    return head(:forbidden) unless @user.authenticate(params[:password])
+    @user = User.find_by(name: params[:user][:name])
+    return head(:forbidden) unless @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
+    redirect_to '/'
   end
 
-  def destroy
-    session[:name] = nil
-    redirect_to '/new'
+  private
+  def sessions_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 
 end
