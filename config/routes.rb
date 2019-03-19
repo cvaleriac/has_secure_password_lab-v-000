6c551afc2ed68164eafa69f7f40811f5d97fad31
valeria campilongo <cvaleriac@hotmail.it>
2019-03-19 15:54:22 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  post '/create' => 'sessions#create'
-  get '/new' => 'sessions#new'
-  get '/home' => 'users#home'
-  post '/destroy' => 'sessions#destroy'
+    get 'users/new'
+    post 'users/create'
+  
+    post 'sessions/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
